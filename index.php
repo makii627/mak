efb1660972667ff45de8a951947ca461fdbf1bdc
Mak Reinhart L. De Castro <116036094+makii627@users.noreply.github.com>
2024-04-21 15:51:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link  rel="stylesheet" href ="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>COFFESHOP</title>
+</head>
+<body>
+    <div class="container">
+    <form action="/search" method="GET">
+        <label for="search"></label>
+        <input type="text" id="search" placeholder="Search.." name="searchbar">
+        <i class="fa-solid fa-magnifying-glass"></i>
+    </form>
+    <div class="menu-con">
+    <div class="menu">
+       <div class="foods">
+       <img src="icon/fast-food.png" alt="All">
+          <h1>All</h1>
+       </div>
+       <div class="drinks">
+       <img src="icon/soda.png" alt="All">
+          <h1>Drinks</h1>
+       </div>
+
+       <div class="burgers">
+       <img src="icon/burger.png" alt="All">
+       <h1>Food</h1>
+       </div>
+       <div class="fries">
+       <img src="icon/fries.png" alt="All">
+       <h1>Snack</h1>
+       </div>
+    </div>
+    </div>
+    <div class="product">
+    <div class="product1">
+        <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+        <div class="button-info">
+        <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product2">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+        <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product3">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product4">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product5">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product6">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product7">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product8">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product9">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product10">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product11">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product12">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product13">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product14">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product15">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product16">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product17">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product18">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product19">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    <div class="product20">
+    <div class="product-name">
+            <h3>Nutty Oat Latte</h3>
+            <h4>Drink</h4>
+        </div>
+    <div class="button-info">
+    <h2>₱39.00</h2>
+    <div class="plus-button">+</div>
+    </div>
+    </div>
+    </div>
+    </div>
+</body>
+</html>
